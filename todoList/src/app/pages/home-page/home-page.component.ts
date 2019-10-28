b9819197514f66d2a6a201464364cf006655df38
Gianpierre Fernandez <Gianpi@Gianpierre-MacBook-Pro.local>
2019-10-28 03:20:37 +0100

    add body and jumbotrom background gradients and add navbar funcionallity to header component

--- todoList/src/app/pages/home-page/home-page.component.ts
+++ todoList/src/app/pages/home-page/home-page.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  listGroup: string[] = ['Build more Projects with Angular', 'Build more projects with Flexbox and Bootstrap', 'Go to the Gym', 'Dance every now and then'];
+  title: string = "To-Do App"
+  listGroup: string[] = ['Build more Projects with Angular', 'Build more projects with Flexbox and Bootstrap', 'Go to the Gym', 'Go out to dance'];
 
   dataInputTyped(dataTyped2: string ) {
     this.listGroup.push(dataTyped2);
