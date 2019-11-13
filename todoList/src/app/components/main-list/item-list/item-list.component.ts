dd66932af3939145129d3301ab708b584e289970
Gianpierre Fernandez <fgianpierre@gmail.com>
2019-11-13 17:23:38 +0100

    add a component for the remove button

--- todoList/src/app/components/main-list/item-list/item-list.component.ts
+++ todoList/src/app/components/main-list/item-list/item-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
   @Input() input: string;
-  
+  @Output() delete = new EventEmitter();
   @Output() done = new EventEmitter<boolean>();
   isClick: boolean = false;
 
@@ -17,6 +17,10 @@ export class ItemListComponent implements OnInit {
   onDone() {
     this.isClick = !this.isClick;
     this.done.emit(this.isClick);
+  }
+
+  onDelete() {
+    this.delete.emit();
   }
 
   
